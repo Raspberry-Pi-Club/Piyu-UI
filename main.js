834b33770cd589fbad0a0343f99d19ebec6e1ae6
Jacob Samro <admin@jsam.in>
2015-06-09 12:52:35 +0530

    working lock & home navigation

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 'use strict';
 
+var applications = '[{"name":"Browser","icon":"icons/default.png","action":"apps.browser"},
+{"name"}
+]';
+
 var system = {
 	homeClose : function(){
 		$("#mainActivityDisplay").slideUp(0).delay(200).html('');
@@ -8,16 +12,19 @@ var system = {
 		$("#mainActivityDisplay").slideDown(200);
 	},
 	generateHomeFooterIcons : function(){
-		return '<span class="app" app="piyu.contacts"><i class="mdi-action-perm-contact-cal"></i></span><span class="app" app="piyu.music"><i class="mdi-av-play-arrow"></i></span><span class="app" app="core.allapps"><i class="mdi-navigation-apps"></i></span><span class="app" app="piyu.browser"><i class="mdi-action-group-work"></i></span><span class="app" app="core.settings"><i class="mdi-action-settings"></i></span>';
+		return '<span class="app app-fix" app="piyu.contacts"><i class="mdi-action-perm-contact-cal"></i></span><span class="app app-fix" app="piyu.music"><i class="mdi-av-play-arrow"></i></span><span class="app app-fix" app="core.allapps"><i class="mdi-navigation-apps"></i></span><span class="app app-fix" app="piyu.browser"><i class="mdi-action-group-work"></i></span><span class="app app-fix" app="core.settings"><i class="mdi-action-settings"></i></span>';
 	},
 	generateHomeMainDisplay : function(){
-		return '';
+		return '<div class="headerDateTime"><div class="leftD"><span class="time"><span class="h">10 : </span><span class="m">45</span><span class="ap">AM</span></span><span class="date">	TUE,&nbsp;MARCH 2019</span></div><div class="rightD"><i class="mdi-file-cloud-queue"></i></div></div>';
 	},
 	generateAppNavigation : function(){
-		return '<span class="navigation" loc="settings"><i class="mdi-hardware-keyboard-control"></i></span><span class="navigation" loc="home"><i class="mdi-action-home"></i></span><span class="navigation" loc="lock"><i class="mdi-action-lock-open"></i></span><span class="navigation" loc="back"><i class="mdi-content-reply"></i></span>';
+		return '<span class="navigation" loc="settings"><i class="mdi-hardware-keyboard-control"></i></span><span class="navigation" loc="home"><i class="mdi-action-home"></i></span><span class="navigation" loc="lock" id="lockHandler"><i class="mdi-action-lock-open" id="lockButton"></i></span><span class="navigation" loc="back"><i class="mdi-content-reply"></i></span>';
 	},
 	body  	: function(data){
 		$('#mainActivityDisplay').html(data);
+	},
+	bodyAppend : function(data){
+		$('#mainActivityDisplay').append(data);
 	},
 	footer : function(data){
 		$('footer').html(data);
@@ -32,13 +39,21 @@ var apps = {
 		system.body("<h2>Contacts App</h2>");
 		system.footer(system.generateAppNavigation());
 		system.homeShow();
-		console.log("Hello");
+		console.log("Contats: Load done");
 	},
 	music : function(){
-
+		system.homeClose();
+		system.body("<h2>Musiq Player</h2>");
+		system.footer(system.generateAppNavigation());
+		system.homeShow();
+		console.log("Player : Load done");
 	},
 	browser : function(){
-
+		system.homeClose();
+		system.body("<h2>Browser</h2>");
+		system.footer(system.generateAppNavigation());
+		system.homeShow();
+		console.log("Browser : Load done");
 	}
 };
 
@@ -67,4 +82,24 @@ $("body").on("click",".app",function(){
 		console.log(name);
 	}
 });
+
 //Body Navigation Click
+$("body").on("click",".navigation",function(){
+	var action = $(this).attr('loc');
+	if(action=='home'){
+		system.homeClose();
+		system.body(system.generateHomeMainDisplay());
+		system.footer(system.generateHomeFooterIcons());
+		system.homeShow();
+	}
+	else if(action=='lock'){
+		system.bodyAppend('<div class="overlay"></div>');
+		$("#lockButton").removeClass('mdi-action-lock-open').addClass('mdi-action-lock');
+		$("#lockHandler").attr('loc','unlock');
+	}
+	else if(action=='unlock'){
+		$('.overlay').remove();
+		$("#lockButton").removeClass('mdi-action-lock').addClass('mdi-action-lock-open');
+		$("#lockHandler").attr('loc','lock');
+	}
+});

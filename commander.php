52a2177015ac8069e6a0225f69005d166759e765
Jacob Samro <admin@jsam.in>
2015-06-18 16:43:12 +0530

    backend logic working

--- commander.php
+++ commander.php
@@ -1,11 +1,36 @@
 <?php
 
-if($_SERVER['REQUEST_METHOD']=="GET"){
+
+if($_SERVER['REQUEST_METHOD']=="GET"&&isset($_GET['command'])){
 	$command = $_GET['command'];
 	
-	$file = fopen("commands.txt", "a");
-		fputs($file,$command."\n");
-	fclose($file);
+if($command==''){
+	die();
+}	
+
+class MyDB extends SQLite3
+   {
+      function __construct()
+      {
+         $this->open('database/interface.db');
+      }
+   }
+$db = new MyDB();
+if(!$db){
+  echo $db->lastErrorMsg();
 }
+else
+{
+
+date_default_timezone_set('Asia/Kolkata');
+$date = date("Y-m-d h:i:s");
+
+  $sql ="INSERT INTO commands (command,date_time) VALUES ('$command', '$date')";
+  $ret = $db->exec($sql);
+  print_r($ret);
+}
+
+
+} #Request
 
 ?>
